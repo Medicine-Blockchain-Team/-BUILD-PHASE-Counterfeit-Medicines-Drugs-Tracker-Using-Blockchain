7cc1fea74190901e5a17b8aaf2b3853553f11f0d
Charitha Bejjam <22bq1a4710@vvit.net>
2025-07-01 17:09:43 +0530

    Delete Drug.js

--- Drug.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DrugSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  history: [String],
-});
-
-module.exports = mongoose.model("Drug", DrugSchema);
